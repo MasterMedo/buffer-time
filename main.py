0c6d0798a4657232f31aae76a8e443be4ecdf424
MasterMedo <mislav.vuletic@gmail.com>
2021-11-10 19:49:38 +0100

    add multiple calendars and handle aware and naive datetime objects -- handle events with and without timezone info

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
 import os.path
 import googlemaps
+import pytz
 
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -13,6 +14,7 @@ BUFFER_TIME_CALENDAR = "Buffer time"
 PREFERRED_TRANSPORT = "driving"
 TRANSPORTS = ["driving", "walking", "bicycling", "transit"]
 EMOJI = {"driving": "🚗", "walking": "🚶", "bicycling": "🚴", "transit": "🚆"}
+CALENDAR_WATCH_LIST = ["Events and activities", "Family"]
 
 
 def main():
@@ -39,90 +41,106 @@ def main():
     if BUFFER_TIME_CALENDAR not in calendars:
         calendars[BUFFER_TIME_CALENDAR] = create_buffer_time_calendar(service)
 
-    # Call the Calendar API
-    print("Getting the upcoming 10 events")
-    start = datetime.datetime.utcnow()
-    events_result = (
-        service.events()
-        .list(
-            calendarId="primary",
-            timeMin=start.isoformat() + "Z",
-            timeMax=(start + datetime.timedelta(days=13-start.weekday())).isoformat() + "Z",
-            maxResults=50,
-            singleEvents=True,
-            orderBy="startTime",
-        )
-        .execute()
-    )
-    events = events_result.get("items", [])
-
-    if not events:
-        print("No upcoming events found.")
-
     with open("key.txt") as f:
         google_maps_key = f.read()
 
     google_maps_client = googlemaps.Client(google_maps_key)
-    origins = ["Radmanovačka ul. 6f, 10000, Zagreb"]
-    for event in events:
-        start_time = event["start"].get("dateTime")
-        if start_time is None:
-            print("event has no start time")
+
+    for calendar_name in calendars:
+        if calendar_name not in CALENDAR_WATCH_LIST:
             continue
 
-        start_time = datetime.datetime.fromisoformat(start_time).timestamp()
-        # TODO: convert start time to integer
-        location = event.get("location")
-        if location is None:
-            print("event has no location")
-            continue
-
-        description_list = []
-        # description.append(
-        #     f"Commute time from {old_location} to {new_location}."
-        # )
-
-        for transport in TRANSPORTS:
-            distance_matrix = google_maps_client.distance_matrix(
-                origins=origins,
-                destinations=[location],
-                mode=transport,
-                arrival_time=start_time,
+        start = datetime.datetime.utcnow()
+        events_result = (
+            service.events()
+            .list(
+                calendarId=calendars[calendar_name],
+                timeMin=start.isoformat() + "Z",
+                timeMax=(
+                    start + datetime.timedelta(days=13 - start.weekday())
+                ).isoformat()
+                + "Z",
+                maxResults=5,
+                singleEvents=True,
+                orderBy="startTime",
             )
-            duration = distance_matrix["rows"][0]["elements"][0].get("duration")
-            if duration is None:
+            .execute()
+        )
+        events = events_result.get("items", [])
+
+        if not events:
+            print("No upcoming events found.")
+            continue
+        # get last location
+        origins = ["Radmanovačka ul. 6f, 10000, Zagreb"]
+        for event in events:
+            start_time = event["start"].get("dateTime")
+            tz = pytz.timezone(event["start"].get("timeZone"))
+            if start_time is None or tz is None:
+                print("event has no start time")
                 continue
 
-            duration_value = duration["value"]
-            duration_text = duration["text"]
-            description_list.append(
-                EMOJI[transport] + " " + transport + " " + duration_text
-            )
-
-            if transport == PREFERRED_TRANSPORT:
-                final_summary = description_list[-1]
-                final_duration_value = duration_value
-                print(f"transport: {transport}")
-                description_list[-1] = "[x] " + description_list[-1]
+            start_time = datetime.datetime.fromisoformat(start_time.strip("Z"))
+            if start_time.tzinfo is None or start_time.tzinfo.utcoffset(start_time) is None:
+                start_time = (
+                    start_time - datetime.datetime(1970, 1, 1)
+                ).total_seconds()
             else:
-                description_list[-1] = "[ ] " + description_list[-1]
+                start_time = start_time.timestamp()
+            # TODO: convert start time to integer
+            location = event.get("location")
+            if location is None:
+                # print("event has no location")
+                continue
 
-        event_id = event["id"]
-        description_list.append(f"Tied to event: {event_id}")
-        description = "\n".join(description_list)
+            description_list = []
+            # description.append(
+            #     f"Commute time from {old_location} to {new_location}."
+            # )
 
-        create_calendar_event(
-            service=service,
-            buffer_time_calendar_id=calendars[BUFFER_TIME_CALENDAR],
-            summary=final_summary,
-            description=description,
-            start_time=datetime.datetime.fromtimestamp(
-                start_time - final_duration_value
-            ),
-            end_time=datetime.datetime.fromtimestamp(start_time),
-            recurrence="",
-            timezone=event["start"]["timeZone"],
-        )
+            for transport in TRANSPORTS:
+                # catch all exceptions here so another transport
+                # method can be tried
+                distance_matrix = google_maps_client.distance_matrix(
+                    origins=origins,
+                    destinations=[location],
+                    mode=transport,
+                    arrival_time=start_time,
+                )
+                duration = distance_matrix["rows"][0]["elements"][0].get(
+                    "duration"
+                )
+                if duration is None:
+                    continue
+
+                duration_value = duration["value"]
+                duration_text = duration["text"]
+                description_list.append(
+                    EMOJI[transport] + " " + transport + " " + duration_text
+                )
+
+                if transport == PREFERRED_TRANSPORT:
+                    final_summary = description_list[-1]
+                    final_duration_value = duration_value
+                    description_list[-1] = "[x] " + description_list[-1]
+                else:
+                    description_list[-1] = "[ ] " + description_list[-1]
+
+            event_id = event["id"]
+            description_list.append(f"Tied to event: {event_id}")
+            description = "\n".join(description_list)
+            create_calendar_event(
+                service=service,
+                buffer_time_calendar_id=calendars[BUFFER_TIME_CALENDAR],
+                summary=final_summary,
+                description=description,
+                start_time=datetime.datetime.fromtimestamp(
+                    start_time - final_duration_value
+                ),
+                end_time=datetime.datetime.fromtimestamp(start_time),
+                recurrence="",
+                timezone=event["start"]["timeZone"],
+            )
 
 
 def list_calendars(service):
