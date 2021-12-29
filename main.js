b13dcc9fdc258c86ea21f06c8c084a7c736ebe81
MasterMedo <mislav.vuletic@gmail.com>
2021-12-29 21:54:44 +0100

    refactor code to satisfy google eslint style

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const BUFFER_TIME_CALENDAR = "Buffer time";
-const EVENT_DESCRIPTION_ID_PREFIX = "Tied to event: ";
+const BUFFER_TIME_CALENDAR = 'Buffer time';
+// const EVENT_DESCRIPTION_ID_PREFIX = 'Tied to event: ';
 const PREFERRED_TRANSPORT = Maps.DirectionFinder.Mode.DRIVING;
-const TRANSPORTS = ["driving", "walking", "bicycling", "transit"];
-const EMOJI = { driving: "🚗", walking: "🚶", bicycling: "🚴", transit: "🚆" };
+// const TRANSPORTS = ['driving', 'walking', 'bicycling', 'transit'];
+const EMOJI = {driving: '🚗', walking: '🚶', bicycling: '🚴', transit: '🚆'};
 const FOUR_HOURS_SECONDS = 4 * 60 * 60;
 const FIVE_HOURS_SECONDS = 5 * 60 * 60;
 const THIRTY_DAYS_SECONDS = 30 * 24 * 60 * 60;
@@ -13,79 +13,129 @@ const BASE_LOCATION = undefined;
 const divmod = (x, y) => [Math.floor(x / y), x % y];
 
 
-function get_duration(origin, destination, arrive_time, transport) {
-  let cache = CacheService.getUserCache();
-  let key = JSON.stringify([origin, destination, transport]);
+/**
+ * Calculates the travel duration between the origin location and the
+ * destination location using the Google Maps API.
+ * Already executed query results are cached.
+ *
+ * @param  {String} origin       Origin location.
+ * @param  {String} destination  Destination location.
+ * @param  {DateTime} arriveTime Destination arrival time.
+ * @param  {String} transport    A mode from `Maps.DirectionFinder.Mode`.
+ * @return {Integer}             Duration in seconds or `undefined`.
+ */
+function getDuration(origin, destination, arriveTime, transport) {
+  const cache = CacheService.getUserCache();
+  const key = JSON.stringify([origin, destination, transport]);
 
-  let duration = cache.get(key);
+  const duration = cache.get(key);
   if (duration !== null) { // if key is not in cache (could be undefined)
     try {
-      let n = Number(duration);
+      const n = Number(duration);
       console.log(`Google maps cache kicked in: ${key}: ${n} seconds`);
       return n;
-    } catch(err) {
+    } catch (err) {
       return undefined; // a path doesn't exist
     }
   }
 
-  let transport_mode = PREFERRED_TRANSPORT;
-  var duration_seconds = Maps.newDirectionFinder()
-    .setOrigin(origin)
-    .setDestination(destination)
-    .setArrive(arrive_time)
-    .setMode(transport)
-    .getDirections()
-    .routes[0]?.legs[0]?.duration?.value;
+  const durationSeconds = Maps.newDirectionFinder()
+      .setOrigin(origin)
+      .setDestination(destination)
+      .setArrive(arriveTime)
+      .setMode(transport)
+      .getDirections()
+      .routes[0]?.legs[0]?.duration?.value;
 
-  if (!!duration_seconds) {
-    cache.put(key, JSON.stringify(duration_seconds), THIRTY_DAYS_SECONDS);
+  if (!!durationSeconds) {
+    cache.put(key, JSON.stringify(durationSeconds), THIRTY_DAYS_SECONDS);
   } else {
-    cache.put(key, "undefined", THIRTY_DAYS_SECONDS);
+    cache.put(key, 'undefined', THIRTY_DAYS_SECONDS);
   }
 
-  return duration_seconds;
+  return durationSeconds;
+}
+
+/**
+ * Formats the commute/travel duration to a readable format. The output is
+ * similar to the following format:
+ *
+ *   🚗 Driving 3 hours 23 minutes
+ *   🚴 Bicycling 56 minutes.
+ *
+ * @param {Integer} durationSeconds  Commute/travel duration in seconds.
+ * @param {String} transport A mode from `Maps.DirectionFinder.Mode`.
+ * @return {String}          Formatted text title.
+*/
+function secondsToText(durationSeconds, transport) {
+  const minutesPre = Math.floor(durationSeconds / 60);
+  const [hours, minutes] = divmod(minutesPre, 60);
+
+  let title = `${EMOJI[transport]} ${transport} `;
+  if (hours) {
+    title += `${hours} hours`;
+  }
+  if (minutes) {
+    title += `${minutes} minutes`;
+  }
+  return title;
 }
 
 
+/**
+ * Creates "buffer time" calendar events that represent commute/travel times
+ * from an origin location to the destination location so that the user is
+ * aware when it's best to get going.
+ *
+ * 1. Creates or fetches the "buffer time calendar".
+ * 2. Fetches all events from all user calendars.
+ * 3. Sorts all events by start time.
+ * 4. For each event calculates commute/travel time from last event's
+ * location to the location of the current event.
+ * 5. Creates a "buffer time" event before each event where the commute
+ * duration is less than 5 hours and the last known location time was less
+ * than 4 hours ago.
+ */
 function main() {
-  var buffer_time_calendars = CalendarApp
-    .getOwnedCalendarsByName(BUFFER_TIME_CALENDAR);
-  if (buffer_time_calendars.length === 0) {
-    var buffer_time_calendar = CalendarApp.createCalendar(BUFFER_TIME_CALENDAR);
-    console.log("Buffer time calendar created!")
+  const bufferTimeCalendars = CalendarApp
+      .getOwnedCalendarsByName(BUFFER_TIME_CALENDAR);
+  let bufferTimeCalendar = undefined;
+  if (bufferTimeCalendars.length === 0) {
+    bufferTimeCalendar = CalendarApp.createCalendar(BUFFER_TIME_CALENDAR);
+    console.log('Buffer time calendar created!');
   } else {
     // TODO: assume user has only one buffer time calendar
-    var buffer_time_calendar = buffer_time_calendars[0];
+    bufferTimeCalendar = bufferTimeCalendars[0];
   }
-  var primary_calendar = CalendarApp.getDefaultCalendar();
+  const primaryCalendar = CalendarApp.getDefaultCalendar();
 
   // TODO: assume all calendars have the same timezone as the default calendar
-  const time_zone = primary_calendar.getTimeZone();
+  const timeZone = primaryCalendar.getTimeZone();
   const today = new Date(new Date().setHours(0, 0, 0, 0));
 
   // google script doesn't provide a method to get first day of the week
-  const week_day = Utilities.formatDate(today, time_zone, "u");
+  const weekDay = Utilities.formatDate(today, timeZone, 'u');
   const yesterday = new Date(today.getTime() - 864e5);
-  const next_sunday = new Date(today.getTime() + 864e5 * (15 - week_day));
-  // console.log("Log: today: " + today);
-  // console.log("Log: yesterday: " + yesterday);
-  // console.log("Log: next_sunday: " + next_sunday);
+  const nextSunday = new Date(today.getTime() + 864e5 * (15 - weekDay));
+  // console.log('Log: today: ' + today);
+  // console.log('Log: yesterday: ' + yesterday);
+  // console.log('Log: nextSunday: ' + nextSunday);
 
   // TODO: assumes all user calendars have the same timezone
   const calendars = CalendarApp.getAllOwnedCalendars();
-  var all_events = [];
-  for (let calendar of calendars) {
-    if (calendar.getId() !== buffer_time_calendar.getId()) {
-      all_events = all_events.concat(calendar.getEvents(yesterday, next_sunday));
+  let allEvents = [];
+  for (const calendar of calendars) {
+    if (calendar.getId() !== bufferTimeCalendar.getId()) {
+      allEvents = allEvents.concat(calendar.getEvents(yesterday, nextSunday));
     }
   }
 
-  all_events.sort(function (event_1, event_2) { // sorts events by start_time
-    let event_1_start_date = event_1.getStartTime();
-    let event_2_start_date = event_2.getStartTime();
-    if ( event_1_start_date < event_2_start_date ) {
+  allEvents.sort(function(event1, event2) { // sorts events by start_time
+    const event1StartDate = event1.getStartTime();
+    const event2StartDate = event2.getStartTime();
+    if (event1StartDate < event2StartDate) {
       return -1;
-    } else if ( event_1_start_date > event_2_start_date ){
+    } else if (event1StartDate > event2StartDate) {
       return 1;
     } else {
       return 0;
@@ -94,50 +144,50 @@ function main() {
 
   // gets user cache, the cache cannot be cleared except key by key
   // this is why we set the cache to 30 days
-  var user_cache = CacheService.getUserCache();
+  const userCache = CacheService.getUserCache();
 
   // let current_date = yesterday;
   // let full_day_event_location = undefined;
-  let last_location = undefined;
-  let last_location_date = undefined;
-  let event_location = undefined;
-  let event_start_date = undefined;
+  let lastLocation = undefined;
+  let lastLocationDate = undefined;
+  let eventLocation = undefined;
+  let eventStartDate = undefined;
 
-  for(const event of all_events) {
+  for (const event of allEvents) {
     // console.log(event.getStartTime())
-    if (!!event_location) {  // if old event location exists
-      last_location = event_location;
-      last_location_date = event.getEndTime();
+    if (!!eventLocation) { // if old event location exists
+      lastLocation = eventLocation;
+      lastLocationDate = event.getEndTime();
     }
 
-    event_start_date = event.getStartTime();
+    eventStartDate = event.getStartTime();
     // TODO: handle full day events
-    // if (event_start_date.getDate() > current_date) {  // if new day has begun
-      // current_date = event_start_date;
-      // full_day_event_location = undefined;
+    // if (eventStartDate.getDate() > current_date) {  // if new day has begun
+    // current_date = eventStartDate;
+    // full_day_event_location = undefined;
     // }
 
-    event_location = event.getLocation();
+    eventLocation = event.getLocation();
     if (event.isAllDayEvent()) { // if event is an all day event
-      // if (!!event_location) {
-      //   full_day_event_location = event_location;
+      // if (!!eventLocation) {
+      //   full_day_event_location = eventLocation;
       // }
       continue;
     }
 
-    if (!!last_location) { // if last location exists
+    if (!!lastLocation) { // if last location exists
       // if more than four hours passed since user was at last location
-      if (event_start_date - last_location_date >= FOUR_HOURS_SECONDS * 1000) {
-        last_location = undefined;
-        last_location_date = undefined;
+      if (eventStartDate - lastLocationDate >= FOUR_HOURS_SECONDS * 1000) {
+        lastLocation = undefined;
+        lastLocationDate = undefined;
       }
     }
-    if (!last_location) { // if last location doesn't exist
-      // last_location = full_day_event_location ?? BASE_LOCATION;
-      last_location = BASE_LOCATION;
+    if (!lastLocation) { // if last location doesn't exist
+      // lastLocation = full_day_event_location ?? BASE_LOCATION;
+      lastLocation = BASE_LOCATION;
     }
 
-    if (!last_location) { // if BASE_LOCATION isn't set
+    if (!lastLocation) { // if BASE_LOCATION isn't set
       continue;
     }
 
@@ -147,45 +197,45 @@ function main() {
       continue;
     }
 
-    let event_id = event.getId();
-    let buffer_time_event_cached = user_cache.get(event_id);
-    if (!!buffer_time_event_cached) { // if event cached
-      // console.log(`Log: Event is cached: ${buffer_time_event_cached}`)
+    const eventId = event.getId();
+    const bufferTimeEventCached = userCache.get(eventId);
+    if (!!bufferTimeEventCached) { // if event cached
+      // console.log(`Log: Event is cached: ${bufferTimeEventCached}`)
       // -------------------------------------------
       // commented block used for clearing the cache
-      // user_cache.remove(buffer_time_event_cached);
+      // userCache.remove(bufferTimeEventCached);
       // continue;
       // -------------------------------------------
-      let [
+      const [
         origin,
         destination,
-        start_date,
-        buffer_time_event_id
-       ] = JSON.parse(buffer_time_event_cached);
+        startDate,
+        bufferTimeEventId,
+      ] = JSON.parse(bufferTimeEventCached);
       if (
-        origin !== last_location
-        || destination !== event.getLocation()
-        || start_date !== event_start_date.toISOString()
-      ) { // if origin, destination or start_date changed
-        if (!!buffer_time_event_id) {
+        origin !== lastLocation ||
+        destination !== event.getLocation() ||
+        startDate !== eventStartDate.toISOString()
+      ) { // if origin, destination or startDate changed
+        if (!!bufferTimeEventId) {
           // sometimes this returns an event object even if the user deleted it
-          buffer_time_event = buffer_time_calendar.getEventById(
-            buffer_time_event_id
+          bufferTimeEvent = bufferTimeCalendar.getEventById(
+              bufferTimeEventId,
           );
           try {
-            buffer_time_event.deleteEvent();
-          } catch(err) {
+            bufferTimeEvent.deleteEvent();
+          } catch (err) {
             // the user deleted the event already
           }
-          user_cache.remove(event.getId());
-          console.log(`Buffer time event deleted from cache ${event.getId()}`)
+          userCache.remove(event.getId());
+          console.log(`Buffer time event deleted from cache ${event.getId()}`);
         }
-      } else { // origin, destination and start_date didn't change
+      } else { // origin, destination and startDate didn't change
         continue;
       }
     } // if event cached
 
-    if (!event_location || event_location === last_location) {
+    if (!eventLocation || eventLocation === lastLocation) {
       continue;
     }
 
@@ -197,94 +247,87 @@ function main() {
       continue;
     }
 
-    // console.log("Log: Last location: " + last_location);
-    // console.log("Log: event_location: " + event_location);
+    // console.log('Log: Last location: ' + lastLocation);
+    // console.log('Log: eventLocation: ' + eventLocation);
 
-    let transport_mode = PREFERRED_TRANSPORT;
-    var duration_seconds = get_duration(
-      last_location,
-      event_location,
-      event_start_date,
-      transport_mode,
+    const transportMode = PREFERRED_TRANSPORT;
+    const durationSeconds = getDuration(
+        lastLocation,
+        eventLocation,
+        eventStartDate,
+        transportMode,
     );
 
-    if (!duration_seconds) { // if a path doesn't exist from one location to the other
+    if (!durationSeconds) { // path doesn't exist
       continue;
     }
 
-    if (duration_seconds > FIVE_HOURS_SECONDS) {
-      user_cache.put(
-        event_id,
-        JSON.stringify([
-          last_location,
-          event_location,
-          event_start_date.toISOString(),
-          undefined,
-        ]),
-        THIRTY_DAYS_SECONDS
-      )
+    if (durationSeconds > FIVE_HOURS_SECONDS) {
+      userCache.put(
+          eventId,
+          JSON.stringify([
+            lastLocation,
+            eventLocation,
+            eventStartDate.toISOString(),
+            undefined,
+          ]),
+          THIRTY_DAYS_SECONDS,
+      );
       continue;
     }
 
-    var [minutes, _] = divmod(duration_seconds, 60);
-    var [hours, minutes] = divmod(minutes, 60);
+    const title = secondsToText(durationSeconds, transportMode);
 
-    let title = `${EMOJI[transport_mode]} ${transport_mode} `;
-    if (hours) {
-      title += `${hours} hours`;
-    }
-    if (minutes) {
-      title += `${minutes} minutes`;
-    }
-
-    const description_list = [
-      "Commute time",
-      `From: ${last_location}`,
-      `To: ${event_location}`,
+    const descriptionList = [
+      'Commute time',
+      `From: ${lastLocation}`,
+      `To: ${eventLocation}`,
     ];
 
     // TODO: go over all transport methods
-    for (let transport of [PREFERRED_TRANSPORT]) {
+    for (const transport of [PREFERRED_TRANSPORT]) {
       if (transport === PREFERRED_TRANSPORT) {
-        description_list.push(`[x] ${title}`)
-        continue;
+        descriptionList.push(`${title}`);
+      } else {
+        // let duration = getDuration(origin, destination, arrive_time, mode);
+        // let text = secondsToText(duration, transport);
+        // descriptionList.push(`${text}`);
       }
-
-      // let duration = get_duration(origin, destination, arrive_time, mode);
-      description_list.push(`[ ] ${title}`)
     }
 
     // adds in the description the even_id of the event it is tied to
-    // description_list.push(EVENT_DESCRIPTION_ID_PREFIX + event_id);
-    let buffer_time_event_description = description_list.join("\n");
+    // descriptionList.push(EVENT_DESCRIPTION_ID_PREFIX + eventId);
+    const bufferTimeEventDescription = descriptionList.join('\n');
 
-    let buffer_time_event_title = title;
-    let buffer_time_event_duration = duration_seconds;
-    let buffer_time_event_start_date = new Date(event_start_date.getTime() - buffer_time_event_duration * 1000);
-    let buffer_time_event_end_date = event_start_date;
+    const bufferTimeEventTitle = title;
+    const bufferTimeEventDuration = durationSeconds;
+    const bufferTimeEventStartDate = new Date(
+        eventStartDate.getTime() - bufferTimeEventDuration * 1000,
+    );
+    const bufferTimeEventEndDate = eventStartDate;
 
-    buffer_time_event = buffer_time_calendar.createEvent(
-      buffer_time_event_title,
-      buffer_time_event_start_date,
-      buffer_time_event_end_date,
-      {
-        description: buffer_time_event_description
-      }
-    )
+    bufferTimeEvent = bufferTimeCalendar.createEvent(
+        bufferTimeEventTitle,
+        bufferTimeEventStartDate,
+        bufferTimeEventEndDate,
+        {
+          description: bufferTimeEventDescription,
+        },
+    );
 
-    console.log(`Buffer time event created: ${buffer_time_event}`)
+    console.log(`Buffer time event created: ${bufferTimeEvent}`);
 
-    buffer_time_event_id = buffer_time_event.getId()
-    console.log(`Buffer time event added to cache: ${event_id}`)
-    user_cache.put(
-      event_id,
-      JSON.stringify([
-        last_location,
-        event_location,
-        event_start_date.toISOString(),
-        buffer_time_event_id,
-      ]),
-      THIRTY_DAYS_SECONDS
-    )
+    bufferTimeEventId = bufferTimeEvent.getId();
+    console.log(`Buffer time event added to cache: ${eventId}`);
+    userCache.put(
+        eventId,
+        JSON.stringify([
+          lastLocation,
+          eventLocation,
+          eventStartDate.toISOString(),
+          bufferTimeEventId,
+        ]),
+        THIRTY_DAYS_SECONDS,
+    );
   }
 }
